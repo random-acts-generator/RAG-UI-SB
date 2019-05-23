ac2c67d5145b7d637317629946ac78554380664e
steven122185 <49796406+steven122185@users.noreply.github.com>
2019-05-22 22:53:04 -0400

    Added the register button and linked it to the app

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,23 @@
 
-/* Add the inner divs to the header-content elements child array*/
+/* For some reason some elements were not properly set as children so I manually appendChild in code*/
 let headerDiv = document.querySelector('.header-content');
 let childHeaderQuoteDiv = document.querySelector('.headerQuoteDiv');
 let childHeaderImgDiv = document.querySelector('.headerImgDiv');
-let childButton = document.querySelector('.ctaBtn');
+let childButton = document.querySelectorAll('.ctaBtn');
 let headerQuote = document.querySelector('.headerQuote');
 let headerIMG = document.querySelector('.headerIMG');
+let buttonDiv = document.querySelector('.buttonDiv');
 childHeaderImgDiv.appendChild(headerIMG);
 childHeaderQuoteDiv.appendChild(headerQuote);
-childHeaderQuoteDiv.appendChild(childButton);
+childHeaderQuoteDiv.appendChild(buttonDiv);
 headerDiv.appendChild(childHeaderQuoteDiv);
 headerDiv.appendChild(childHeaderImgDiv);
 
 // Use the button element we already retrieved to set an event listener
-childButton.addEventListener('click', function() {
+childButton[0].addEventListener('click', function() {
     window.open("https://random-acts-generator.netlify.com/login");
+})
+childButton[1].addEventListener('click', function() {
+    window.open("https://random-acts-generator.netlify.com");
 })
 
